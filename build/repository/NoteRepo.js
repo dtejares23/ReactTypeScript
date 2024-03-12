eb87fcc930a67b1ee2fdfc2f08e97efc19d02c96
dtejares23 <daryltejares@gmail.com>
2024-03-12 17:09:14 +0800

    update in repository

--- build/repository/NoteRepo.js
+++ build/repository/NoteRepo.js
@@ -1,19 +1,46 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NoteRepo = void 0;
+const Note_1 = require("../src/model/Note");
 class NoteRepo {
     save(note) {
-        try {
-        }
-        catch (err) {
-            throw new Error("Method not implemented");
-        }
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield Note_1.Note.create({
+                    name: note.name,
+                    description: note.description
+                });
+            }
+            catch (err) {
+                throw new Error("Method not implemented");
+            }
+        });
     }
     update(note) {
-        try {
-        }
-        catch (err) {
-        }
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const new_note = yield Note_1.Note.findOne({
+                    where: {
+                        id: note.id,
+                    },
+                });
+                if (!new_note) {
+                    throw new Error("You FAILED to create note!");
+                }
+            }
+            catch (err) {
+                throw new Error("Method not implemented");
+            }
+        });
     }
     delete(note) {
         try {
--- repository/NoteRepo.ts
+++ repository/NoteRepo.ts
@@ -3,7 +3,7 @@ import { Note } from "../src/model/Note";
 interface INoteRepo {
     save(note: Note): Promise<void>;
     update(note: Note): Promise<void>;
-    delete(note: number): Promise<void>;
+    delete(noteId: number): Promise<void>;
     retrieveById(noteId: number): Promise<Note>;
     retrieveAll(): Promise<Note[]>;
 }
@@ -30,33 +30,53 @@ export class NoteRepo implements INoteRepo {
             if(!new_note) {
                 throw new Error("You FAILED to create note!")
             }
+            new_note.name = note.name;
+            new_note.description = note.description;
+
+            await new_note.save();
         }
         catch(err) {
             throw new Error("Method not implemented");
         }
     }
-    delete(note: number): Promise<void> {
+    async delete(noteId: number): Promise<void> {
         try {
-
+            const new_note = await Note.findOne({
+                where:{
+                    id: noteId,
+                },
+            });
+            if(!noteId) {
+                throw new Error("Note not found!")
+            }
+            await new_note.destroy();
         }
         catch {
-
+            throw new Error("Failed to delete note!")
         }
     }
-    retrieveById(noteId: number): Promise<Note> {
+    async retrieveById(noteId: number): Promise<Note> {
         try {
-
+            const new_note = await Note.findOne({
+                where:{
+                    id: noteId,
+                },
+            });
+            if(!noteId) {
+                throw new Error("Note not found!")
+            }
+            return new_note;
         }
         catch {
-
+            throw new Error("Failed to retrieve note!")
         }
     }
-    retrieveAll(): Promise<Note[]> {
+    async retrieveAll(): Promise<Note[]> {
         try {
-
+            return await Note.findAll();
         }
         catch {
-
+            throw new Error("Failed to retrieve ALL note!")
         }
     }
 }
